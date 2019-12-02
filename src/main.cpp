eb55551a2ec90aa6b2b0d5ad95a6463b6be76491
johnfraymundo <johnfraymundo@gmail.com>
2019-12-02 16:40:49 -0600

    ayy lmao

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,65 @@
 #include <iostream>
+#include <string> 
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+struct point {
+    point() : x( 0 ), y( 0 ) {}
+    point( int x, int y ) : x( x ), y( y ) {}
+
+    int x;
+    int y;
+};
+
+struct GameMap{
+};
+
+struct organismList{
+
+};
+
+class organism{
+    organism(int energy, std::string type, point location): energy(energy), location(location), type(type){}
+
+    public:
+    int energy; 
+    std::string type;
+    point location; 
+
+    void move(const point&){
+    }
+    
+    void gainEnergy(){
+    }
+
+    void consume(){
+    }
+
+};
+
+class plant : organism{
+
+};
+
+class Herbavore : organism{
+
+};
+
+class Omnivore : organism{
+
+};
+
+void gameLoop(){
+    //Do game stuff here 
+}
+
+int main(){
+    int iterations; 
+    std::cout << "Welcome to the game! Please input the number of iterations you would like to do.";
+    std::cin >> iterations; 
+    std::cout << "You've selected: " << iterations << " many iterations, beginning!";
+
+    int n = 0;
+    while (n < iterations - 1){
+        gameLoop( );
+        ++n; 
+    }
 }
