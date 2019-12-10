049b660a9383add84a8a19427ebffe50223d8864
johnfraymundo <johnfraymundo@gmail.com>
2019-12-10 02:06:13 -0600

    attributes

--- fromtxt.cpp
+++ fromtxt.cpp
@@ -1,125 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdio.h>
+
 using namespace std;
 
-// Class to define the properties
-class Contestant {
-public:
-// Instance variables
-char Name[50] = {};
-int Age, Ratings;
-Contestant() {};
-~Contestant() {};
-// Function declaration of input() to input info
-int input();
-void setName(std::string in);
-// Function declaration of output_highest_rated() to
-// extract info from file Data Base
-int output_highest_rated();
-};
-void Contestant::setName(std::string in) {
-memset(Name, 0, sizeof(Name));
-if (in.size() < 50) {
-memcpy(Name, in.c_str(), in.size());
-}
-else {
-memcpy(Name, in.c_str(), sizeof(Name) -1);
-}
-
-}
-// Function definition of input() to input info
-int Contestant::input()
+int main ()
 {
-// Object to write in file
-ofstream file_obj;
+  ifstream myfile ("sequence.txt");
+  if (!myfile)
+  {
+    perror ("File error: ");
+    system ("pause");
+    exit (EXIT_FAILURE);
+  }
+  const int NUM_TRUCKS = 5;
+  int id = 0, time1 = 0, time2 = 0, i = 0;
+  string type;
+  while (myfile >> id >> type >> time1 >> time2 && i++ < NUM_TRUCKS)
+  {
+    // use the input
+    cout << id << '\t' << type << '\t' << time1 << '\t' << time2 << '\n';
+  }
 
-// Opening file in append mode
-file_obj.open("Input.txt", ios::app);
-
-// Object of class contestant to input data in file
-Contestant obj;
-
-// Feeding appropriate data in variables
-string str = "Micheal";
-int age = 18, ratings = 2500;
-
-// Assigning data into object
-obj.setName(str);
-obj.Age = age;
-obj.Ratings = ratings;
-
-// Writing the object's data in file
-file_obj.write((char*)&obj, sizeof(obj));
-
-// Feeding appropriate data in variables
-str = "Terry";
-age = 21;
-ratings = 3200;
-
-// Assigning data into object
-obj.setName(str);
-obj.Age = age;
-obj.Ratings = ratings;
-
-// Writing the object's data in file
-file_obj.write((char*)&obj, sizeof(obj));
-
-return 0;
-}
-
-// Function definition of output_highest_rated() to
-// extract info from file Data Base
-int Contestant::output_highest_rated()
-{
-// Object to read from file
-ifstream file_obj;
-
-// Opening file in input mode
-file_obj.open("Input.txt", ios::in);
-
-// Object of class contestant to input data in file
-
-// max to store maximum ratings
-int max = 0;
-
-// Highest_rated stores the name of highest rated contestant
-string Highest_rated;
-bool keeplooping = true;
-// Checking till we have the feed
-while (keeplooping) {
-Contestant obj;
-
-// Reading from file into object "obj"
-file_obj.read((char*)&obj, sizeof(obj));
-if (!file_obj.eof()) {
-// Assigning max ratings
-if (obj.Ratings > max) {
-max = obj.Ratings;
-Highest_rated = obj.Name;
-}
-}
-else {
-keeplooping = false;
-}
-}
-
-// Output is the highest rated contestant
-std::cout << Highest_rated;
-return 0;
-}
-
-// Driver code
-int main()
-{
-// Creating object of the class
-Contestant object;
-
-// Inputting the data
-object.input();
-
-// Extracting the max rated contestant
-object.output_highest_rated();
-
-return 0;
+  system ("pause");
+  return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <map>
 #include <vector>   
@@ -9,6 +10,20 @@
 struct point {
     point() : x( 0 ), y( 0 ) {}
     point( int x, int y ) : x( x ), y( y ) {}
+
+    point& operator+=( point const& other )
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    point& operator-=( point const& other ) 
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
 
     int x;
     int y;
@@ -25,13 +40,16 @@ void parseTxT(std::string fileinput){
     }
 }
 
-class thing{
+struct thing{
 public:
     using thing_id = char; 
     
     thing() : id_(){}
     thing(thing_id id) : id_(id){}
-    virtual ~thing() {}
+
+    virtual ~thing() {
+        std::cout << "Destroying thing";
+    }
 
     thing_id  id() const
     {
@@ -59,6 +77,7 @@ public:
         return nullptr;
     }
 
+    public:
     thing_id id_; 
 };
 
@@ -76,54 +95,136 @@ struct GameMap{
     std::vector<std::vector<thing*>> gamemap;
 };
 
-class TerrainObject : thing{
-    TerrainObject() : thing('t'){}
-    
+struct TerrainObject : thing{
+    TerrainObject() : thing(){}
 };
 
-class organism : thing{
-    organism(int energy, std::string type, point location): energy(energy), location(location), type(type){}
-
+struct organism : thing{
     public:
-    int energy; 
-    std::string type;
-    point location; 
+    organism(thing_id id, int energy, std::string type, point location): energy(energy), location(location), thing(id){}
 
-    void move(const point&){
+    int energy; 
+    point location;
+    thing_id id;
+    std::string type;
+
+    virtual std::string kind() const { 
+        return type; 
+    }
+
+    void move(const point& destination){
+        this -> location = destination;
     }
     
     void gainEnergy(){
     }
 
-    void consume(){
+    virtual void consume( organism& org){
+        this -> energy += org.energy;
     }
 
 };
 
-struct OrganismList{
-    OrganismList() : orgVector(){} 
+struct plant : organism{
+    plant(char id, int energy, std::string type, point location, int regrowth, bool passable) : organism(id, energy, type, location), regrow_coeff(regrowth){}
+
+    std::string kind() const override{
+        return "plant";
+    }
+
+    std::unique_ptr<thing> create( thing_id id, int energy, std::string type, point location, int regrowth ) const 
+    {
+        return std::make_unique<plant>(id, energy, location, regrowth);
+    }
+
+    void gainEnergy(){
+        int reg = regrow_coeff;
+        while (reg != 0){
+            --reg;
+        }
+
+        this -> passable = true;     
+    } 
+
     public:
-    std::vector<organism> orgVector; 
+    int regrow_coeff;
+    bool passable;
+
+};
+
+struct herbivore : organism{
+    herbivore(char id, int energy, std::string type, point location, std::vector<thing_id> prey) : organism(id, energy, type, location){} 
+
+    std::string kind() const override{
+        return "herbivore";
+    }
+
+    std::unique_ptr<thing> create( thing_id id, int energy, std::string type, point location, std::vector<thing_id> prey) const 
+    {
+        return std::make_unique<herbivore>(id, energy, type, location, prey);
+    } 
+
+    private:
+    std::vector<thing_id> prey; 
+
+
+};
+
+struct omnivore : organism{
+    omnivore(char id, int energy, std::string type, point location, std::vector<thing_id> prey) : organism(id, energy, type, location){}
+    
+    std::string kind() const override{
+        return "omnivore";
+    }
+
+    std::unique_ptr<thing> create( thing_id id, int energy, std::string type, point location, std::vector<thing_id> prey) const 
+    {
+        return std::make_unique<omnivore>(id, energy, type, location, prey);
+    }  
+
+    private:
+    std::vector<thing_id> prey; 
+
+};
+
+struct OrganismList{
+    OrganismList() : orgVector(orgVector){} 
+
+    void addToList(std::unique_ptr<organism> org){
+        this -> orgVector.push_back(org); 
+    }
+    void removeHelper(){
+        int i = 0; 
+        for()
+    }
+    void removeFromList(point p){
+        this -> orgVector.erase(); 
+    }
+    public:
+    std::vector<std::unique_ptr<organism>> orgVector; 
 };
 
 struct OrganismDirectory{
-    OrganismDirectory() : odirectory(){}
+    //vector of organism templates 
+    OrganismDirectory()
+    {
+        this -> odirectory.push_back(std::make_unique<plant>());
+        this -> odirectory.push_back(std::make_unique<herbivore>());
+        this -> odirectory.push_back(std::make_unique<omnivore>());
+    }
 
+    organism* find( std::string const& d ) const
+    {
+        for ( auto& org : this->odirectory )
+            if ( org->kind() == d )
+                return org.get();
+
+        return nullptr;
+    }
     
-    public:
-    std::vector<organism> odirectory; 
 
-};
-
-class plant : organism{
-
-};
-
-class Herbavore : organism{
-
-};
-
-class Omnivore : organism{
+    private:
+    std::vector<std::unique_ptr<organism>> odirectory; 
 
 };
 
